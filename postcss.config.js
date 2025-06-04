b90f003c0c46a6b71cc5c134ea94df4cb9c6d00a
Karthikeyan-Mathivanan <karthikeyan.mathivanan.1420@gmail.com>
2025-06-04 23:53:15 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    plugins: [
-      require('@tailwindcss/postcss'), // Use the new postcss plugin
-      require('autoprefixer'),
-    ],
-  };
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
